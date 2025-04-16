63adc3fb376b6ed8daed0c91773280aa3d1d1b56
LucasNGRE <lucas12negre@gmail.com>
2025-04-16 09:30:45 +0200

    add the island perimeter task

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of an island in a grid.
+
+    Args:
+        grid (list of list of int): A 2D grid representing the island (1) and water (0).
+
+    Returns:
+        int: The perimeter of the island.
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check all four directions
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:  # Up
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:  # Left
+                    perimeter -= 2
+
+    return perimeter

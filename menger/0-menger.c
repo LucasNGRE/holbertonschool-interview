3cb01e14e47bfdfff84c3da3a8cd203a6d6e57ec
LucasNGRE <lucas12negre@gmail.com>
2026-01-23 14:03:40 +0100

    betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -4,34 +4,34 @@
 
 static int is_filled(int x, int y)
 {
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (0);
-        x /= 3;
-        y /= 3;
-    }
-    return (1);
+	while (x > 0 || y > 0)
+	{
+		if (x % 3 == 1 && y % 3 == 1)
+			return (0);
+		x /= 3;
+		y /= 3;
+	}
+	return (1);
 }
 
 void menger(int level)
 {
-    int size, x, y;
+	int size, x, y;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    size = pow(3, level);
+	size = pow(3, level);
 
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            if (is_filled(x, y))
-                putchar('#');
-            else
-                putchar(' ');
-        }
-        putchar('\n');
-    }
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (is_filled(x, y))
+				putchar('#');
+			else
+				putchar(' ');
+		}
+		putchar('\n');
+	}
 }
